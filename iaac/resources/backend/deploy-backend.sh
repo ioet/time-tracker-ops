da31071f05e8bc2bd82955d0be0a5569580769d7
fabidick22 <fabidick22@hotmail.es>
2020-04-02 17:34:15 -0500

    Update deploy-backend.sh file

--- iaac/resources/backend/deploy-backend.sh
+++ iaac/resources/backend/deploy-backend.sh
@@ -3,4 +3,6 @@
 # Region: southcentralus
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 RESOURCE_GROUP="eys-training-api"
+
+#TODO: Validate ARM Template
 az group deployment create --name deploy-"$DATE_WITH_TIME" --resource-group "$RESOURCE_GROUP" --template-file template.json --parameters parameters.json
