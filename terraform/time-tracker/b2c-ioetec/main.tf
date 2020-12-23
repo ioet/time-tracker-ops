73cec9b52c47e724d917d7c7c8fb05d4b71ceab3
Diego <diegotony1396@gmail.com>
2020-12-23 18:37:14 -0500

    Create User and Group

--- terraform/time-tracker/b2c-ioetec/main.tf
+++ terraform/time-tracker/b2c-ioetec/main.tf
@@ -3,19 +3,19 @@ provider "azuread" {
   tenant_id = "cc925a5d-9644-4a4f-8d99-0bee49aadd05"
 }
 
-# module "test_user" {
-#   source              = "../../resources/user"
-#   given_name          = "Test"
-#   surname             = "User"
-#   user_principal_name = "test_user@ioetec.com"
-#   mail_nickname       = "test_user"
-# }
+module "test_user" {
+  source              = "../../resources/user"
+  given_name          = "Test"
+  surname             = "User"
+  user_principal_name = "test_user@ioetec.onmicrosoft.com"
+  mail_nickname       = "test_user"
+}
 
 
-# module "test_group" {
-#   source              = "../../resources/group"
-#   name    = "TestUsers"
-#   members = [
-#     module.test_user.object_id,
-#   ]
-# }
+module "test_group" {
+  source              = "../../resources/group"
+  name    = "TestUsers"
+  # members = [
+  #   module.test_user.object_id,
+  # ]
+}
