bfb5310351539afb20ac35ad41fad499fb7479c7
Diego <diegotony1396@gmail.com>
2020-12-23 18:36:40 -0500

    Terraform initializacion

---
+++ terraform/resources/group/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type    = string
+  default = ""
+}
+
+variable "members" {
+  type    = list
+  default = []
+}
---
+++ terraform/resources/user/output.tf
@@ -0,0 +1,3 @@
+output "object_id" {
+  value = azuread_user.new_user.object_id
+}
