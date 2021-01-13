e43c6a23180dd1b808cd5164adf25de991f78edc
Diego <diegotony1396@gmail.com>
2021-01-13 16:34:01 -0500

    feat(config): Added app-service.plan resource

---
+++ terraform/resources/azurem/app-service-plan/variables.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "kind" {
+  type = string
+}
+variable "reserved" {
+  type = bool
+}
+
+variable "tier" {
+  type = string
+}
+
+variable "size" {
+  type = string
+}
